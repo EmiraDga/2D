317f4258bf4fcb4d85e79e56f4e7e5772e1d015a
EmiraDga <emiradga888@gmail.com>
2021-09-15 22:00:56 +0200

    second commit

--- rect.js
+++ rect.js
@@ -1,6 +1,33 @@
-let rect = document.getElementById("square-little")
-
 function myFunction() {
-    document.getElementById("square-little").style.transition = "all 2s";
+    var boxB = document.getElementById("squareB");
+    var boxY = document.getElementById("squareY");
+    var box = document.getElementById("squareR");
+    var pos = 0;
+    var myVar = setInterval(move, 5);
 
+    function move() {
+        for (let i = 0; i < 9; i++) {
+            if (pos == 600) {
+                clearInterval(myVar);
+            } else {
+                pos++
+                boxB.style.top = pos + 'px'
+                boxB.style.bottom = pos + 'px'
+                box.style.bottom = pos + 'px'
+                box.style.right = pos + 'px'
+                boxY.style.left = pos + 'px'
+                boxY.style.top = pos + 'px'
+
+            }
+
+        }
+        //carac()
+    }
+
+
+
+
+    function carac() {
+
+    }
 }
